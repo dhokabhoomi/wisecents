1d9d720d45f910df6c1540ec3ef3f1a461319871
dhokabhoomi <bhoomidhoka@gmail.com>
2025-06-23 16:31:38 +0530

    Setup App component with transaction state and handlers

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import CategoryFilter from "./components/CategoryFilter/CategoryFilter";
+import SummaryDashboard from "./components/SummaryDashboard/SummaryDashboard";
+import TransactionForm from "./components/TransactionForm/TransactionForm";
+import TransactionList from "./components/TransactionList/TransactionList";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [transactions, setTransactions] = useState([]);
+  const [editTx, setEditTx] = useState(null);
+
+  const addTransaction = (newTr) => {
+    setTransactions([newTr, ...transactions]);
+  };
+
+  const deleteTransaction = (id) => {
+    setTransactions(transactions.filter((tx) => tx.id !== id));
+  };
+
+  const startEdit = (tx) => {
+    setEditTx(tx);
+  };
+
+  const updateTransaction = (updatedTx) => {
+    setTransactions((prev) =>
+      prev.map((tx) => (tx.id === updatedTx.id ? updatedTx : tx))
+    );
+    setEditTx(null);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="container mt-4">
+      <h2>WiseCents - Expense Tracker</h2>
+      <SummaryDashboard transactions={transactions} />
+      <TransactionForm
+        onAdd={addTransaction}
+        onUpdate={updateTransaction}
+        editTx={editTx}
+      />
+      <CategoryFilter />
+      <TransactionList
+        transactions={transactions}
+        onDelete={deleteTransaction}
+        onEdit={startEdit}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
