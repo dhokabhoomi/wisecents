c4f3b6753ffa2c51a4422c7342f00495f92f4cbc
dhokabhoomi <bhoomidhoka@gmail.com>
2025-06-26 15:48:35 +0530

    Installed bootstrap and changed title in index.html

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
-import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import App from "./App";
